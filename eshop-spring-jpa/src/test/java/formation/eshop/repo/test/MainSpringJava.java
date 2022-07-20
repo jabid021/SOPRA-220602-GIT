122c90f2aeee7909be4a6ce6fdc264fee1029e68
Eric <eric>
2022-07-20 17:24:49 +0200

    Main example minimal

--- eshop-spring-jpa/src/test/java/formation/eshop/repo/test/MainSpringJava.java
+++ eshop-spring-jpa/src/test/java/formation/eshop/repo/test/MainSpringJava.java
@@ -3,11 +3,21 @@ package formation.eshop.repo.test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import formation.eshop.config.ApplicationConfig;
+import formation.eshop.model.Produit;
+import formation.eshop.repo.IFournisseurRepository;
+import formation.eshop.repo.IProduitRepository;
 
 public class MainSpringJava {
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
+		IFournisseurRepository fournisseurRepo = context.getBean(IFournisseurRepository.class);
+		IProduitRepository produitRepo = context.getBean(IProduitRepository.class);
+
+		Produit iphone = new Produit("IPhone 13");
+
+		produitRepo.save(iphone);
 
 		context.close();
 
--- eshop-spring-jpa/src/test/java/formation/eshop/repo/test/MainSpringXml.java
+++ eshop-spring-jpa/src/test/java/formation/eshop/repo/test/MainSpringXml.java
@@ -2,11 +2,22 @@ package formation.eshop.repo.test;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import formation.eshop.model.Produit;
+import formation.eshop.repo.IFournisseurRepository;
+import formation.eshop.repo.IProduitRepository;
+
 public class MainSpringXml {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:application-context.xml");
 
+		IFournisseurRepository fournisseurRepo = context.getBean(IFournisseurRepository.class);
+		IProduitRepository produitRepo = context.getBean(IProduitRepository.class);
+
+		Produit iphone = new Produit("IPhone 13");
+
+		produitRepo.save(iphone);
+		
 		context.close();
 	}
 
